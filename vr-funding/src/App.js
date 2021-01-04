577cbc8412ee73cf91733052b23cc8d521495184
s-munro <68410363+s-munro@users.noreply.github.com>
2021-01-04 13:27:01 -0500

    syntax fix

--- vr-funding/src/App.js
+++ vr-funding/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import NavigationBar from "./components/NavigationBar";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
-import ProjectPage from './components/ProjectPage';
+import ProjectPage from "./components/ProjectPage";
 
 import "./App.css";
 
@@ -14,7 +14,7 @@ function App() {
       <div className="App">
         <div>Hello, world</div>
       </div>
-      <Route path="/projects" component={ProjectPage}
+      <Route path="/projects" component={ProjectPage} />
       <Route path="/login" component={Login} />
       <Route path="/signup" component={Signup} />
     </Router>
