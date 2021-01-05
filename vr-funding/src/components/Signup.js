712b676feb36cd447e205b1fd4dc5d316e0e328b
mikayla-bryant <mikaylabryant25@gmail.com>
2021-01-05 18:55:27 -0500

    Centered text inputs using inline CSS styling

--- vr-funding/src/components/Signup.js
+++ vr-funding/src/components/Signup.js
@@ -3,31 +3,96 @@ import React from 'react';
 const Signup = () => {
   return (
     <div>
-      <h1>Create a SIXR Account</h1>
-      <form>
-        <p>Select your role:</p>
+      {/* Note: I will be using inline CSS temporarily to avoid merge conflicts until we can combine it on one stylesheet */}
+      <form
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          border: '3px black solid',
+          margin: '2%',
+        }}
+      >
+        <h1
+          style={{
+            width: '50%',
+            margin: 'auto',
+            fontSize: '1.5rem',
+            textAlign: 'center',
+          }}
+        >
+          Create a SIXR Account
+        </h1>
 
-        {/*Radio Button 1: Fundraiser*/}
-        <input type='radio' id='fundraiser' name='role' value='1' />
-        <label for='fundraiser'>Fundraiser</label>
+        <section style={{ width: '50%', margin: 'auto', textAlign: 'center' }}>
+          <p>Select your role:</p>
 
-        {/*Radio Button 2: Funder*/}
-        <input type='radio' id='funder' name='role' value='2' />
-        <label for='funder'>Funder</label>
+          {/*Radio Button 1: Fundraiser*/}
+          <input type='radio' id='fundraiser' name='role' value='1' />
+          <label for='fundraiser'>Fundraiser</label>
 
+          {/*Radio Button 2: Funder*/}
+          <input type='radio' id='funder' name='role' value='2' />
+          <label for='funder'>Funder</label>
+        </section>
         {/*Text Input Forms Based on API EndPoints: First Name, Last Name, Email, Password*/}
-        <label htmlFor='first_name'>First Name</label>
-        <input id='first_name' type='text'></input>
-        <label htmlFor='last_name'>Last Name</label>
-        <input name='last_name' id='last_name' type='text'></input>
-        <label htmlFor='email'>Email</label>
-        <input name='email' id='email' type='text'></input>
-        <label htmlFor='password'>Password</label>
-        <input name='password' id='password' type='password'></input>
-        <button>Create Account</button>
+        <section
+          style={{
+            width: '50%',
+            margin: 'auto',
+            textAlign: 'center',
+          }}
+        >
+          <label htmlFor='first_name'>First Name</label>
+          <input id='first_name' type='text'></input>
+          <label htmlFor='last_name'>Last Name</label>
+          <input name='last_name' id='last_name' type='text'></input>
+        </section>
+        <label
+          style={{
+            width: '50%',
+            margin: 'auto',
+          }}
+          htmlFor='email'
+        >
+          Email Address
+        </label>
+        <input
+          style={{
+            width: '50%',
+            margin: 'auto',
+            textAlign: 'center',
+          }}
+          name='email'
+          id='email'
+          type='text'
+        ></input>
+        <label
+          style={{
+            width: '50%',
+            margin: 'auto',
+          }}
+          htmlFor='password'
+        >
+          Password (must be at least 6 characters)
+        </label>
+        <input
+          style={{
+            width: '50%',
+            margin: 'auto',
+            textAlign: 'center',
+          }}
+          name='password'
+          id='password'
+          type='password'
+        ></input>
+        <button style={{ width: '20%', margin: '2% auto' }}>
+          Create Account
+        </button>
       </form>
     </div>
   );
 };
+
+export default Signup;
 
 export default Signup;
