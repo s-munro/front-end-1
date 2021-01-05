b9db87f69b64a9f3a222b83f4e3669e70ed662a7
Your Name <you@example.com>
2021-01-05 17:24:27 -0600

    2 - States Made, Dashboards setup

--- vr-funding/src/App.js
+++ vr-funding/src/App.js
@@ -1,21 +1,33 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import React, { useState, useEffect } from 'react';
 import NavigationBar from "./components/NavigationBar";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
 import ProjectPage from "./components/ProjectPage";
 import DashPageA from "./components/Dashboard_Fundrasier";
 import DashPageB from "./components/Dashboard_Funder";
-
-
 import "./App.css";
 
+
 function App() {
+
+  const [login, setLogin] = useState(false);
+  const [role, setRole] = useState();
+
+  const changeLoginState = (evt) => {
+    setLogin(true);
+  };
+
+  const setRoleState = (evt) => {
+     setRole(evt.target === "A" ? evt.target = "B" : evt.target = "A",
+      )
+    };
+
   return (
     <Router>
       <NavigationBar />
       <div className="App">
-        <div>Hello, world</div>
+        <div></div>
       </div>
 
       <DashPageA/>
