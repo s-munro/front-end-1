efa0f151d2f9d7309a55ca13f8e529f2f92f5a50
s-munro <68410363+s-munro@users.noreply.github.com>
2021-01-05 17:35:32 -0500

    reducer and thunk action fleshed out

---
+++ vr-funding/src/actions/index.js
@@ -0,0 +1,18 @@
+import axiosWithAuth from "../utils/axiosWithAuth";
+
+export const FETCHING_PROJECTS_START = "FETCHING_PROJECTS_START";
+export const FETCHING_PROJECTS_SUCCESS = "FETCHING_PROJECTS_SUCCESS";
+export const FETCHING_PROJECTS_FAIL = "FETCHING_PROJECTS_FAIL";
+
+export const fetchProjects = () => (dispatch) => {
+  dispatch({ type: FETCHING_PROJECTS_START });
+  axiosWithAuth("/projects")
+    .then((res) => {
+      console.log("res: ", res);
+      dispatch({ type: FETCHING_PROJECTS_SUCCESS, payload: res.data });
+    })
+    .catch((err) => {
+      console.log("err: ", err);
+      dispatch({ type: FETCHING_PROJECTS_FAIL, payload: err.response });
+    });
+};
