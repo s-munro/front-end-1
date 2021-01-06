e16fb11c07e5be7d85313b322adc22cb33c57a81
Brennen O'Connor <brennenoconnor@Brennens-MacBook-Pro.local>
2021-01-05 20:05:30 -0500

    login form without styling

--- vr-funding/src/components/Login.js
+++ vr-funding/src/components/Login.js
@@ -1,7 +1,24 @@
 import React from "react";
 
 const Login = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Sign In</h1>
+      <form>
+        <div className="login-forms">
+          <label htmlFor="email">Email</label>
+          <input id="email" type="text"></input>
+          <br></br>
+          <label htmlFor="password">Password</label>
+          <input name="password" id="password" type="password"></input>
+        </div>
+
+        <button>Sign in to SIXR</button>
+        <br></br>
+        <a href="">Forgot your password?</a>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
