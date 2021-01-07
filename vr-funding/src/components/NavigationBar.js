4c61dcc67ca04343e1e8bd3f080e611caa6f4213
s-munro <68410363+s-munro@users.noreply.github.com>
2021-01-07 18:19:47 -0500

    logout working

--- vr-funding/src/components/NavigationBar.js
+++ vr-funding/src/components/NavigationBar.js
@@ -1,17 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 import "../App.css";
 import styled from "styled-components";
 
-
-const NavStyled = styled.div` 
+const NavStyled = styled.div`
   color: ${(pr) => pr.theme.secondaryColor};
   font-weight: bold;
   display: flex;
   justify-content: space-evenly;
   align-content: space-between;
-  
 `;
 
 const BtnStyled = styled.div`
@@ -20,32 +18,42 @@ const BtnStyled = styled.div`
   border: 2px solid ${(pr) => pr.theme.primaryColor};
   box-shadow: 0px 1px 6px -2px #807f7f;
   border-radius: 8px;
-  padding:10px;
+  padding: 10px;
   font-size: 1.3rem;
   font-weight: bold;
-  
 `;
 
-
 const NavigationBar = () => {
+  const history = useHistory();
+
+  const handleLogout = (e) => {
+    localStorage.removeItem("role");
+    localStorage.removeItem("id");
+    localStorage.removeItem("token");
+    // history.push("/login");
+  };
   return (
     <NavStyled>
       {/* <nav className="nav-bar"> */}
-        <Link to="/" style={{textDecoration:"none",}}>
+      <Link to="/" style={{ textDecoration: "none" }}>
         <BtnStyled>Home</BtnStyled>
-        </Link>
+      </Link>
 
-        <Link to="/projects" style={{textDecoration:"none",}}>
+      <Link to="/projects" style={{ textDecoration: "none" }}>
         <BtnStyled>Projects</BtnStyled>
-        </Link>
+      </Link>
 
-        <Link to="/login" style={{textDecoration:"none",}}>
+      <Link to="/login" style={{ textDecoration: "none" }}>
         <BtnStyled>Login</BtnStyled>
-        </Link>
+      </Link>
 
-        <Link to="/signup" style={{textDecoration:"none",}}>
+      <Link to="/signup" style={{ textDecoration: "none" }}>
         <BtnStyled>Sign up</BtnStyled>
-        </Link>
+      </Link>
+
+      <Link to="/login" style={{ textDecoration: "none" }}>
+        <BtnStyled onClick={handleLogout}>Logout</BtnStyled>
+      </Link>
       {/* </nav> */}
     </NavStyled>
   );
