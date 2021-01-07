30baba47e259ffd7b4791f9cb10eab0ecc697aad
Sam Munro <68410363+s-munro@users.noreply.github.com>
2021-01-07 15:06:02 -0500

    Revert "Revert "navbar minor styling, added project link""

--- vr-funding/src/components/NavigationBar.js
+++ vr-funding/src/components/NavigationBar.js
@@ -2,15 +2,26 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import "../App.css";
+import "../NavigationBar.css";
 
 const NavigationBar = () => {
   return (
     <nav className="nav-bar">
-      <Link to="/">Home</Link>
+      <Link to="/" style={{ textDecoration: "none" }}>
+        <li>Home</li>
+      </Link>
 
-      <Link to="/login">Login</Link>
+      <Link to="/projects" style={{ textDecoration: "none" }}>
+        <li>Projects</li>
+      </Link>
 
-      <Link to="/signup">Sign up</Link>
+      <Link to="/login" style={{ textDecoration: "none" }}>
+        <li>Login</li>
+      </Link>
+
+      <Link to="/signup" style={{ textDecoration: "none" }}>
+        <li>Sign Up</li>
+      </Link>
     </nav>
   );
 };
