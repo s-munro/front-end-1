b0ceb12fee880ae12b096867e4e1de9b8ec046c4
s-munro <68410363+s-munro@users.noreply.github.com>
2021-01-07 14:15:45 -0500

    setRole added to state, added to both dashboards on render

--- vr-funding/src/actions/index.js
+++ vr-funding/src/actions/index.js
@@ -4,6 +4,8 @@ export const FETCHING_PROJECTS_START = "FETCHING_PROJECTS_START";
 export const FETCHING_PROJECTS_SUCCESS = "FETCHING_PROJECTS_SUCCESS";
 export const FETCHING_PROJECTS_FAIL = "FETCHING_PROJECTS_FAIL";
 
+export const SET_ROLE = "SET_ROLE";
+
 export const fetchProjects = () => (dispatch) => {
   dispatch({ type: FETCHING_PROJECTS_START });
   axiosWithAuth()
@@ -16,4 +18,8 @@ export const fetchProjects = () => (dispatch) => {
       console.log("err: ", err);
       dispatch({ type: FETCHING_PROJECTS_FAIL, payload: err.response });
     });
+};
+
+export const setRole = () => {
+  return { type: SET_ROLE, payload: localStorage.getItem("role") };
 };
