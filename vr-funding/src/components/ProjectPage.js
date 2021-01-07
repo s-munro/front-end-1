3c011cc6afd4bcbbb75c09966e6defa62835afa8
s-munro <68410363+s-munro@users.noreply.github.com>
2021-01-07 12:59:45 -0500

    projectpage loading data from globalstate, globalstate projects coming from api call

--- vr-funding/src/components/ProjectPage.js
+++ vr-funding/src/components/ProjectPage.js
@@ -1,33 +1,45 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useRouteMatch } from "react-router-dom";
 
+import { connect } from "react-redux";
+import { fetchProjects } from "../actions/index";
 
-export default function ProjectList(props) {
+const ProjectList = (props) => {
   const { projects } = props;
-  
+
   const { url } = useRouteMatch();
+
+  useEffect(() => {
+    return props.fetchProjects();
+  }, []);
 
   return (
     <div className="project-list">
-      {projects.map(
-
-        (project) => (
-          <div className="project-card" key={project.id}>
-            
-              {/* <Link to={`${url}/${project.id}`}> */}
-              <div className="project-card"><p>{project.title}</p></div>
-              {/* </Link> */}
-
-            <div className="project-card"><p>{project.discription}</p></div>
-
-            <div className="project-card"><p>Click Here to Fund!</p></div>
+      {projects.map((project) => (
+        <div className="project-card" key={project.id}>
+          {/* <Link to={`${url}/${project.id}`}> */}
+          <div className="project-card">
+            <p>{project.title}</p>
           </div>
-         
-            )
-        )
-      }
+          {/* </Link> */}
+
+          <div className="project-card">
+            <p>{project.discription}</p>
+          </div>
+
+          <div className="project-card">
+            <p>Click Here to Fund!</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
-}
-
-
+};
+const mapStateToProps = (state) => {
+  return {
+    isLoading: state.isLoading,
+    errorText: state.errorText,
+    projects: state.projects,
+  };
+};
+export default connect(mapStateToProps, { fetchProjects })(ProjectList);
